5671f90c4b7f93ac0fb9f423567dcbe24784d996
Linh Nguyen Bui Tai <linh.nguyenbuitai>
2023-04-25 18:06:48 +0700

    add UI nav

--- src/layout/Nav/index.tsx
+++ src/layout/Nav/index.tsx
@@ -1,17 +1,25 @@
 import MyButton from "../../components/MyButton";
 import UserIcon from "../../components/UserIcon";
+import { RiMessage3Line } from "react-icons/ri";
+import { TiGroup } from "react-icons/ti";
+import { HiBellAlert } from "react-icons/hi2";
+import { MdContacts } from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
+import { AiOutlineSetting } from "react-icons/ai";
 
+;
 function Nav() {
   return (
     <>
-      <UserIcon className="w-1/3" size={"h-10 w-10"}  status="active"/>
-      <div className="flex flex-col justify-center flex-grow">
-        <MyButton theme="primary" className="w-1/3" label={"abc"} />
-        <MyButton theme="primary" className="w-1/3" label={"abc"} />
-        <MyButton theme="primary" className="w-1/3" label={"abc"} />
-        <MyButton theme="primary" className="w-1/3" label={"abc"} />
+      <UserIcon className="w-1/3" size={"h-10 w-10"} status="active" />
+      <div className="flex flex-col justify-evenly flex-grow py-24">
+        <MyButton theme="primary" label={<HiBellAlert size={"2em"} />} />
+        <MyButton theme="primary" label={<RiMessage3Line size={"2em"} />} />
+        <MyButton theme="primary" label={<TiGroup size={"2em"} />} />
+        <MyButton theme="primary" label={<MdContacts size={"2em"} />} />
+        <MyButton theme="primary" label={<BsThreeDots size={"2em"} />} />
       </div>
-      <MyButton theme="primary" className="w-1/3" label={"abc"} />
+      <MyButton theme="primary" label={<AiOutlineSetting size={"2em"} />} />
     </>
   );
 }
